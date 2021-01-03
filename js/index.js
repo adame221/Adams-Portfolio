8200c551b4cceb32191e2e5badc4614c87520c7c
Adam Ely <adam.d.ely92@gmail.com>
2021-01-03 12:02:31 -0600

    Added detailed comments

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+/** 
+  * This class is used to interact with the index.html page
+  * 
+  * Author: Adam Ely
+  * Date: 01/03/2021
+*/
+//Creating constant variables to get certain elements from the page
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+//Adding an event listener to open the navigation menu
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+//For each link, that you click on, close the menu.
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,36 @@
+/** 
+  * This class is used to interact with the portfolio-item.html page
+  * 
+  * Author: Adam Ely
+  * Date: 01/03/2021
+*/
 $(document).ready(function () {
     var myObj;
 
+    //Making an ajax request to get project infomation
     $.ajax ({
         type: "GET",
         url: "./json/projects.json",
         dataType: "json",
+
+        //If the request was a success
         success: function (pros) { 
             myObj = pros;
             console.log(pros);
 
+            //Creating variables to hold information that was returned from the ajax request
             let url = new URL(window.location.href);
             let params = new URLSearchParams(url.search);
             let q = params.get('project') // 'mdn query string'
         
+            //Getting a list of projects from the request.
             var projects = myObj.project;
             var projectFound = false;
         
+            //Looping through each project
             for (var i = 0; i < projects.length; i++) {
         
+                //If there was a project id found
                 if (q == projects[i].id) {
                     projectFound = true;
                     $("#projectTitle").html(myObj.project[i].name);
@@ -28,15 +41,18 @@ $(document).ready(function () {
                     $("#description2").html(myObj.project[i].description2);
                     $("#lastUpdated").html(myObj.project[i].lastUpdated);
                     
+                    //If the project has a github link
                     if(myObj.project[i].gitHub.length > 1) {
                         $("#description2").append('<p>Please click <a href="' + myObj.project[i].gitHub + '" target="_blank">this link</a> to view the source code for this project.</p>');
                     }
 
+                    //If the project has page to display
                     if(myObj.project[i].iframePath.length > 1 && myObj.project[i].video.length < 1) {
                         $("#description1").append("<br /><a href='" + myObj.project[i].iframePath + "' target='_blank'>Click Here For Full Page View</a>");
                         $("#description1").append("<br /><iframe src='" + myObj.project[i].iframePath + "' height='500' width='800'></iframe>");
                     }
 
+                    //If the project has a video instead of an iframe
                     if(myObj.project[i].video.length < 1) {
                         $("#videoP").css("display", "none");
                     } else {
@@ -45,6 +61,7 @@ $(document).ready(function () {
                 }
             }
 
+            //If the a project was found
             if (projectFound) {
                 $("#projectFound").css("display", "block");
                 $("#projectNotFound").css("display", "none");
